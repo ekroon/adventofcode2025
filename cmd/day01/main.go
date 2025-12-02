1ef1b59325f6e1d83defd3662317ce7ea3dbd91c
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2025-12-02 23:22:08 +0100

    Solve day01 part2: count zero crossings with tick simulation

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -36,8 +36,36 @@ func part1(lines []string) int {
 }
 
 func part2(lines []string) int {
-	// TODO: implement
-	return 0
+	dial := 50
+	password := 0
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		direction := line[0]
+		amount, _ := strconv.Atoi(line[1:])
+
+		for i := 0; i < amount; i++ {
+			if direction == 'L' {
+				dial--
+				if dial < 0 {
+					dial = 99
+				}
+			} else if direction == 'R' {
+				dial++
+				if dial > 99 {
+					dial = 0
+				}
+			}
+
+			if dial == 0 {
+				password++
+			}
+		}
+	}
+
+	return password
 }
 
 func main() {
