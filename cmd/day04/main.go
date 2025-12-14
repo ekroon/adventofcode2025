fb27e8574329d2cca44f88e3a308c7c015a0d8d2
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2025-12-14 21:00:23 +0100

    Add day04 part2: iteratively remove unstable paper stacks

--- cmd/day04/main.go
+++ cmd/day04/main.go
@@ -42,8 +42,59 @@ func part1(lines []string) int {
 }
 
 func part2(lines []string) int {
-	// TODO: implement
-	return 0
+	rows := len(lines)
+	if rows == 0 {
+		return 0
+	}
+	cols := len(lines[0])
+
+	// Convert to mutable grid
+	grid := make([][]byte, rows)
+	for r := range rows {
+		grid[r] = []byte(lines[r])
+	}
+
+	// 8 directions: N, NE, E, SE, S, SW, W, NW
+	dirs := [][2]int{
+		{-1, 0}, {-1, 1}, {0, 1}, {1, 1},
+		{1, 0}, {1, -1}, {0, -1}, {-1, -1},
+	}
+
+	totalRemoved := 0
+
+	for {
+		// Find all stacks to remove this round
+		var toRemove [][2]int
+		for r := range rows {
+			for c := range cols {
+				if grid[r][c] != '@' {
+					continue
+				}
+				adjacent := 0
+				for _, d := range dirs {
+					nr, nc := r+d[0], c+d[1]
+					if nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '@' {
+						adjacent++
+					}
+				}
+				if adjacent < 4 {
+					toRemove = append(toRemove, [2]int{r, c})
+				}
+			}
+		}
+
+		if len(toRemove) == 0 {
+			break
+		}
+
+		// Remove all marked stacks
+		for _, pos := range toRemove {
+			grid[pos[0]][pos[1]] = '.'
+		}
+		totalRemoved += len(toRemove)
+	}
+
+	return totalRemoved
 }
 
 func main() {
